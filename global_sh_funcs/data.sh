718c4073f6955a114bc4c6e1c78330f37727c3c3
Jakub Halik <oranzovydzusaja@proton.me>
2026-03-03 19:04:19 +0100

    kss thousands

--- global_sh_funcs/data.sh
+++ global_sh_funcs/data.sh
@@ -1,16 +1,22 @@
+path_to_encodings_tables_and_such=$HOME/d/g/g/encodings
+path_to_notes=$HOME/d/g/g/obsidian-notes
+
 thousands() {
   echo "thousand, million, billion, trillion, quadrillion, quintillion, sextillion, septillion, octillion, nonillion, decillion, undecillion, duodecillion, tredecillion, quattuordecillion, quindecillion, sexdecillion, septendecillion, octodecillion, novemdecillion, vigintillion"
 }
 
 ks() {thousands}
 
+thousandss() {
+  cat $path_to_notes/high_numbers
+}
+kss() {thousandss}
+
 perm() { 
   echo "| Permission  | Binary | Octal |\n| ----------- | ------ | ----- |\n| Read (r)    | \`100\`  | \`4\`   |\n| Write (w)   | \`010\`  | \`2\`   |\n| Execute (x) | \`001\`  | \`1\`   |" 
 }
 
 rights() {perm}
-
-path_to_encodings_tables_and_such=$HOME/d/g/g/encodings
 
 ansi() {
   cat $path_to_encodings_tables_and_such/ANSI.md
